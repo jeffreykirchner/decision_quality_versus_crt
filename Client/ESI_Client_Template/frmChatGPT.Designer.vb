4114b81eca72efa2db250116e881611995040f39
jkirchner@gmail.com <jkirchner@gmail.com>
2025-05-19 17:15:14 -0700

    Refactor UI and build signing processes
    Replaced direct `signtool.exe` commands in `PostBuildEvent` with batch files (`sign_client.bat` and `sign_server.bat`) in project files.
    
    Enhanced `frmChatGPT`:
    - Added new `Label` and `Button` controls.
    - Refactored `cmdSend_Click` with error handling and UI updates.
    - Introduced `clear_screen` and `cmdReset_Click` methods.
    - Improved form initialization and key handling logic.
    
    Updated `frmChatGPT.Designer.vb` to reflect new controls and layout adjustments.

--- Client/ESI_Client_Template/frmChatGPT.Designer.vb
+++ Client/ESI_Client_Template/frmChatGPT.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class frmChatGPT
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,12 +20,13 @@ Partial Class frmChatGPT
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmChatGPT))
         Me.cmdSend = New System.Windows.Forms.Button()
         Me.textPrompt = New System.Windows.Forms.TextBox()
         Me.rtbResponse = New System.Windows.Forms.RichTextBox()
+        Me.cmdReset = New System.Windows.Forms.Button()
         Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
@@ -35,9 +36,9 @@ Partial Class frmChatGPT
         Me.cmdSend.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.cmdSend.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.cmdSend.Location = New System.Drawing.Point(783, 3)
+        Me.cmdSend.Location = New System.Drawing.Point(753, 3)
         Me.cmdSend.Name = "cmdSend"
-        Me.cmdSend.Size = New System.Drawing.Size(169, 28)
+        Me.cmdSend.Size = New System.Drawing.Size(97, 28)
         Me.cmdSend.TabIndex = 0
         Me.cmdSend.Text = "Send"
         Me.cmdSend.UseVisualStyleBackColor = True
@@ -51,7 +52,7 @@ Partial Class frmChatGPT
         Me.textPrompt.Location = New System.Drawing.Point(3, 3)
         Me.textPrompt.MaxLength = 200
         Me.textPrompt.Name = "textPrompt"
-        Me.textPrompt.Size = New System.Drawing.Size(774, 26)
+        Me.textPrompt.Size = New System.Drawing.Size(744, 26)
         Me.textPrompt.TabIndex = 2
         Me.textPrompt.Text = "Type your question here."
         Me.textPrompt.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
@@ -71,18 +72,32 @@ Partial Class frmChatGPT
         Me.rtbResponse.TabIndex = 3
         Me.rtbResponse.Text = ""
         '
+        'cmdReset
+        '
+        Me.cmdReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdReset.Location = New System.Drawing.Point(856, 3)
+        Me.cmdReset.Name = "cmdReset"
+        Me.cmdReset.Size = New System.Drawing.Size(96, 28)
+        Me.cmdReset.TabIndex = 5
+        Me.cmdReset.Text = "Reset"
+        Me.cmdReset.UseVisualStyleBackColor = True
+        '
         'TableLayoutPanel1
         '
         Me.TableLayoutPanel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.TableLayoutPanel1.ColumnCount = 2
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.74757!))
-        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.25243!))
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.92497!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.07503!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 102.0!))
         Me.TableLayoutPanel1.Controls.Add(Me.cmdSend, 1, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.textPrompt, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.cmdReset, 2, 0)
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 434)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
         Me.TableLayoutPanel1.Size = New System.Drawing.Size(955, 37)
@@ -92,7 +107,7 @@ Partial Class frmChatGPT
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(979, 483)
+        Me.ClientSize = New System.Drawing.Size(973, 479)
         Me.ControlBox = False
         Me.Controls.Add(Me.TableLayoutPanel1)
         Me.Controls.Add(Me.rtbResponse)
@@ -109,5 +124,6 @@ Partial Class frmChatGPT
     Friend WithEvents cmdSend As Button
     Friend WithEvents textPrompt As TextBox
     Friend WithEvents rtbResponse As RichTextBox
+    Friend WithEvents cmdReset As Button
     Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
 End Class
--- Client/ESI_Client_Template/frmChatGPT.vb
+++ Client/ESI_Client_Template/frmChatGPT.vb
@@ -11,31 +11,10 @@ Public Class frmChatGPT
     Dim messages As New List(Of ChatMessage) From {
         New SystemChatMessage("You are a helpful AI assistant that answers questions concisely.")
     }
-    Private Async Sub cmdSend_Click(sender As Object, e As EventArgs) Handles cmdSend.Click
 
-        cmdSend.Enabled = False
-        textPrompt.Enabled = False
-        cmdSend.Text = "Working..."
+    Dim clientOptions As New OpenAIClientOptions
 
-        'display prompt in textbox
-        rtbResponse.AppendText(textPrompt.Text)
-        rtbResponse.SelectionStart = rtbResponse.Text.Length - textPrompt.Text.Length
-        rtbResponse.SelectionLength = textPrompt.Text.Length
-
-        rtbResponse.SelectionAlignment = HorizontalAlignment.Right
-        rtbResponse.AppendText(vbCrLf & vbCrLf)
-
-        rtbResponse.Refresh()
-
-        messages.Add(New UserChatMessage(textPrompt.Text))
-
-        Dim clientOptions As New OpenAIClientOptions
-        'clientOptions.Endpoint =
-        Dim client As New AzureOpenAIClient(New Uri(azure_openai_endpoint), New AzureKeyCredential(azure_openai_key))
-
-        Dim chat_client = client.GetChatClient(azure_openai_model)
-
-        Dim options As New ChatCompletionOptions With {
+    Dim options As New ChatCompletionOptions With {
             .Temperature = 1.0F,
             .MaxOutputTokenCount = 800,
             .TopP = 1.0F,
@@ -43,59 +22,131 @@ Public Class frmChatGPT
             .PresencePenalty = 0.0F
         }
 
-        ' Create the chat completion request
-        Dim completion As ChatCompletion = Await chat_client.CompleteChatAsync(messages, options)
+    Dim client As New AzureOpenAIClient(New Uri(azure_openai_endpoint), New AzureKeyCredential(azure_openai_key))
 
-        ' Print the response
-        Dim content As String = "No response received."
-        If completion IsNot Nothing Then
-            Dim v = JsonConvert.SerializeObject(completion.Content(0))
+    Private Async Sub cmdSend_Click(sender As Object, e As EventArgs) Handles cmdSend.Click
+        Try
+            cmdSend.Enabled = False
+            cmdReset.Enabled = False
+            textPrompt.Enabled = False
+            cmdSend.Text = "Working..."
 
-            Dim jsonResult As Dictionary(Of String, String) = JsonConvert.DeserializeObject(Of Dictionary(Of String, String))(v)
-            content = jsonResult("Text")
+            'display prompt in textbox
+            rtbResponse.AppendText(textPrompt.Text)
+            rtbResponse.SelectionStart = rtbResponse.Text.Length - textPrompt.Text.Length
+            rtbResponse.SelectionLength = textPrompt.Text.Length
 
-            messages.Add(New AssistantChatMessage(content))
-        End If
+            rtbResponse.SelectionAlignment = HorizontalAlignment.Right
+            rtbResponse.AppendText(vbCrLf & vbCrLf)
 
-        rtbResponse.AppendText(content)
-        rtbResponse.SelectionStart = rtbResponse.Text.Length - content.Length
-        rtbResponse.SelectionLength = content.Length
+            rtbResponse.Refresh()
 
-        rtbResponse.SelectionAlignment = HorizontalAlignment.Left
-        rtbResponse.ScrollToCaret()
+            messages.Add(New UserChatMessage(textPrompt.Text))
 
-        rtbResponse.AppendText(vbCrLf & vbCrLf)
+            'clientOptions.Endpoint =
 
-        'send message to server
-        Dim str As String = ""
-        str &= textPrompt.Text & ";"
-        str &= content & ";"
+            Dim chat_client = client.GetChatClient(azure_openai_model)
 
-        frmClient.AC.sendMessage("07", str)
+            ' Create the chat completion request
+            Dim completion As ChatCompletion = Await chat_client.CompleteChatAsync(messages, options)
 
-        'clear text box
-        textPrompt.Clear()
-        textPrompt.Enabled = True
-        textPrompt.Focus()
-        cmdSend.Enabled = True
-        cmdSend.Text = "Send"
+            ' Print the response
+            Dim content As String = "No response received."
+            If completion IsNot Nothing Then
+                Dim v = JsonConvert.SerializeObject(completion.Content(0))
+
+                Dim jsonResult As Dictionary(Of String, String) = JsonConvert.DeserializeObject(Of Dictionary(Of String, String))(v)
+                content = jsonResult("Text")
+
+                messages.Add(New AssistantChatMessage(content))
+            End If
+
+            rtbResponse.AppendText(content)
+            rtbResponse.SelectionStart = rtbResponse.Text.Length - content.Length
+            rtbResponse.SelectionLength = content.Length
+
+            rtbResponse.SelectionAlignment = HorizontalAlignment.Left
+            rtbResponse.ScrollToCaret()
+
+            rtbResponse.AppendText(vbCrLf & vbCrLf)
+
+            'send the reponse to the server.
+            Dim str As String = ""
+            str = textPrompt.Text & ";"
+            str &= content & ";"
+
+            frmClient.AC.sendMessage("07", str)
+
+            'clear the text box
+            textPrompt.Clear()
+            textPrompt.Enabled = True
+            textPrompt.Focus()
+            cmdSend.Enabled = True
+            cmdSend.Text = "Send"
+
+            cmdReset.Enabled = True
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
 
         'MessageBox.Show(reply)
     End Sub
 
     Private Sub frmChatGPT_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            clear_screen()
+        Catch ex As Exception
+
+        End Try
 
     End Sub
 
     Private Sub textPrompt_KeyUp(sender As Object, e As KeyEventArgs) Handles textPrompt.KeyUp
-        If e.KeyCode = Keys.Enter Then
-            cmdSend_Click(sender, e)
-        End If
+        Try
+            If e.KeyCode = Keys.Enter Then
+                cmdSend_Click(sender, e)
+            End If
+        Catch ex As Exception
+            appEventLog_Write("error :", ex)
+        End Try
+
     End Sub
 
-    Private Sub textPrompt_Click(sender As Object, e As EventArgs) Handles textPrompt.Click
-        If textPrompt.Text = "Type your question here." Then
+    Public Sub clear_screen()
+        Try
+            rtbResponse.Clear()
             textPrompt.Clear()
-        End If
+            textPrompt.Focus()
+
+            rtbResponse.Text = "Hi, I am a chat bot and here to help you. Ask me anything, however keep in mind that I cannot see your screen. This means you will have to explain what you see before asking me a question about it."
+            rtbResponse.SelectionStart = 0
+            rtbResponse.SelectionLength = rtbResponse.Text.Length
+            rtbResponse.SelectionAlignment = HorizontalAlignment.Center
+
+            rtbResponse.AppendText(vbCrLf & vbCrLf)
+
+            messages = New List(Of ChatMessage) From {
+                New SystemChatMessage("You are a helpful AI assistant that answers questions concisely.")
+            }
+            cmdSend.Enabled = True
+        Catch ex As Exception
+            appEventLog_Write("error :", ex)
+        End Try
+
     End Sub
+
+    Private Sub cmdReset_Click(sender As Object, e As EventArgs) Handles cmdReset.Click
+        Try
+            clear_screen()
+
+            Dim str As String = ""
+            str = "RESET CHAT BOT" & ";"
+            str &= "" & ";"
+
+            frmClient.AC.sendMessage("07", str)
+        Catch ex As Exception
+            appEventLog_Write("error :", ex)
+        End Try
+    End Sub
+
 End Class
